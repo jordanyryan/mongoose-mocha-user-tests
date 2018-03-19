57b25a05396f138885269bb3d3e45c3af910c470
Jordany Rosas <jordanyryan@gmail.com>
2018-03-19 17:56:07 -0400

    Implemented update test, and refactored promises in delete_test and update_test

--- test/delete_test.js
+++ test/delete_test.js
@@ -9,48 +9,30 @@ describe('Deleting a user', () => {
       .then(() => done());
   });
 
-  it('model instance remove', (done) => {
-    joe.remove()
-      .then(() => {
+  function assertRemoved(operation, done) {
+    operation
+    .then(() => {
         User.findOne({name: 'Joe'})
       })
       .then((user) => {
         assert(!user)
         done();
       })
+  }
+
+  it('model instance remove', (done) => {
+    assertRemoved(joe.remove(), done);
   });
 
   it('class method remove', (done) => {
-    User.remove({name: 'Joe'})
-    .then(() => {
-        User.findOne({name: 'Joe'})
-      })
-      .then((user) => {
-        assert(!user)
-        done();
-      })
-
+    assertRemoved(User.remove({name: 'Joe'}), done)
   });
 
   it('class method findOneAndRemove', (done) => {
-    User.findOneAndRemove({name: 'Joe'})
-    .then(() => {
-        User.findOne({name: 'Joe'})
-      })
-      .then((user) => {
-        assert(!user)
-        done();
-      })
+    assertRemoved(User.findOneAndRemove({name: 'Joe'}),done);
   });
 
   it('class method findByIdAndRemove', (done) => {
-    User.findByIdAndRemove(joe._id)
-    .then(() => {
-        User.findOne({name: 'Joe'})
-      })
-      .then((user) => {
-        assert(!user)
-        done();
-      })
+    assertRemoved(User.findByIdAndRemove(joe._id),done);
   });
 });

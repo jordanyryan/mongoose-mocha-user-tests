7e9c46f828812c995fdc94a3f25b505ff9ba1864
Jordany Rosas <jordanyryan@gmail.com>
2018-03-19 18:06:20 -0400

    Added a postCount field to user model, and implemented an update operator test to check postcount was raised

--- src/user.js
+++ src/user.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  name: String
+  name: String,
+  postCount: {type: Number, default: 0}
 });
 
 const User = mongoose.model('user', UserSchema);
--- test/update_test.js
+++ test/update_test.js
@@ -4,7 +4,7 @@ const User = require('../src/user');
 describe('Update records', () => {
   let joe;
   beforeEach((done) => {
-    joe = new User({name: 'Joe'});
+    joe = new User({name: 'Joe', postCount: 0});
     joe.save()
       .then(() => done());
   });
@@ -40,6 +40,17 @@ describe('Update records', () => {
 
   it('a model class can find a record with id and update', (done) => {
     assertName(User.findByIdAndUpdate({_id: joe._id}, {name: 'Alex'}), done);
+  });
+
+  it('A user can have their post count increment by 1', (done) => {
+    User.update({name: 'Joe'}, {$inc: {postCount: 1}})
+      .then(() => {
+        User.findOne({name: 'Joe'})
+          .then(user => {
+            assert(user.postCount === 1);
+            done();
+          });
+      });
   });
 
 })

4e13557cc4a47aa8b76bd24f858923e44c39c7e9
Jordany Rosas <jordanyryan@gmail.com>
2018-03-19 19:25:29 -0400

    created tests for creating subdocments, adding subdocuments to existing document, and remove existing subdocuments

--- src/user.js
+++ src/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const PostSchema = require('./post');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
@@ -10,7 +11,8 @@ const UserSchema = new Schema({
     },
     required: [true, 'Name is required.']
   },
-  postCount: {type: Number, default: 0}
+  postCount: {type: Number, default: 0},
+  posts: [PostSchema]
 });
 
 const User = mongoose.model('user', UserSchema);

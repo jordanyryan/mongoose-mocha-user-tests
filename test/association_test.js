32af8f177bc25b25db79706f6fbe074347d14458
Jordany Rosas <jordanyryan@gmail.com>
2018-03-20 16:57:56 -0400

    Added comment and blogposts schemas and models, and set up associations in each model

---
+++ test/association_test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const User = require('../src/user');
+const Comment = require('../src/comment');
+const BlogPost = require('../src/blogpost');
+
+describe('Associations', () => {
+  it('', (done) => {
+
+  });
+});
